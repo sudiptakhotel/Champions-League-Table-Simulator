abb1dfc56d013137b7e149598b4346c77bc375c6
Sudipta Khotel <sudiptakhotel4@gmail.com>
2026-01-29 21:23:45 +0530

    initialize spring boot project

---
+++ src/main/java/com/springboot/ChampionsLeagueableSimulator/ChampionsLeagueableSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ChampionsLeagueableSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionsLeagueableSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionsLeagueableSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ChampionsLeagueableSimulator/ChampionsLeagueableSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ChampionsLeagueableSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionsLeagueableSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
